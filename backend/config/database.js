ac0815731a50bc69fb1c2af53c04c77ecb1888e4
JCrosby0 <jcrosby@internode.on.net>
2021-03-23 11:51:02 +0800

    WIP hosting backend on heroku, test deploy from push

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,4 @@
+module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -5,18 +6,10 @@
       settings: {
         uri: env('DATABASE_URI'),
       },
-      settings: {
-        host: env('DATABASE_HOST', null),
-        srv: env.bool('DATABASE_SRV', 'true'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', null),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
-      },
-
       options: {
         ssl: true,
       },
     },
   },
 });
+
