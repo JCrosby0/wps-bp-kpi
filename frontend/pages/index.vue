64ea26b879cb15035109b971d88101cdfc626840
JCrosby0 <jcrosby@internode.on.net>
2021-03-24 14:38:25 +0800

    added info to index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,10 +2,40 @@
   <div class="container">
     <div class="main">
       <p class="description">
-        A summary of Strategies for Improvement, Measuring Success and
-        Performance Targets along with indicators showing progress towards
-        achieving the Business Plan's goals
+        This portal provides a queriable summary of Strategies for Improvement,
+        Success Measurements and Performance Targets along with indicators
+        showing progress towards achieving the Business Plan's goals
       </p>
+      <p class="notification">
+        Please note: the server sleeps when idle, and may take up to 20 seconds
+        to respond when waking up. Please be patient!
+      </p>
+      <div class="left">
+        <h6>Known Issues:</h6>
+        <ul>
+          <li>
+            Refreshing a page may display a page not found error. Go back to
+            wps-bp-kpi.netlify.app and Use the menu to navigate back to the
+            desired page.
+          </li>
+          <li>
+            Emails are not currently being sent out when with account
+            registration validation or password reset requests. Contact Joe for
+            manual account validation.
+          </li>
+        </ul>
+        <h6>Planned Features:</h6>
+        <ul>
+          <li>Strategies, Measurements and Targets searchable / filterable</li>
+          <li>View Indicators by latest update</li>
+          <li>
+            Add additional tab to indicators: evidence (links to supporting /
+            relevant activities)
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer">
       <p>Created by Crosby Solutions, 2021</p>
     </div>
   </div>
@@ -22,15 +52,21 @@
 }
 .main {
   padding: 1rem;
+  flex: 1 0 calc(100% - 1rem);
 }
-
+.footer {
+  flex: 0 0 1rem;
+  font-size: 0.8rem;
+}
 .description {
   background: black;
   padding: 1rem;
   border: 1px grey solid;
   color: #efefef;
 }
-
+.left {
+  text-align: left;
+}
 .subtitle {
   font-weight: 300;
   font-size: 2rem;
@@ -39,7 +75,7 @@
   padding-bottom: 15px;
 }
 
-.links {
-  padding-top: 15px;
+.notification {
+  padding: 1rem;
 }
 </style>
